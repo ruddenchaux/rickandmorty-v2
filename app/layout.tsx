54135bf27f773d6a047e8e065fad09c9bc430677
Federico Dainelli <f.r.dainelli@gmail.com>
2024-08-18 17:01:00 +0200

    docs: update a readme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import { CssBaseline } from '@mui/material';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
-import { Metadata } from 'next';
+import { Metadata, Viewport } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Layout from './layout/index';
@@ -9,6 +9,24 @@ import theme from './theme';
 
 export const metadata: Metadata = {
   description: 'Discover the Rick and Morty world',
+  icons: [{ rel: 'apple-touch-icon', url: 'logo.svg' }],
+  manifest: 'manifest.json',
+  keywords: [
+    'Rick and Morty',
+    'Characters',
+    'Episodes',
+    'Locations',
+    'Next.js',
+    'Material-UI',
+    'Cloudflare',
+    'Cloudflare Pages',
+  ],
+};
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: 'black',
 };
 
 const inter = Inter({ subsets: ['latin'] });
